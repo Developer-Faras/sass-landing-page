0b547b2c974a7404d2d2490a581acb3384d6cb2f
Developer-Faras <forasuddin.freelancer@gmail.com>
2022-02-01 23:51:11 +0600

    Colpmate Rsponsive Navbar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 console.log("No Js Errors");
+
+
+
+// Navbar Toggler
+let toggler = document.getElementById('nav-toggler');
+toggler.addEventListener('click', function () {
+    toggler.classList.toggle('active');
+});
