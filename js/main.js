d09652436ee1120c285986dea899e77a1180b548
Developer-Faras <forasuddin.freelancer@gmail.com>
2022-02-02 23:25:25 +0600

    Complate Services Card

--- js/main.js
+++ js/main.js
@@ -7,3 +7,17 @@ let toggler = document.getElementById('nav-toggler');
 toggler.addEventListener('click', function () {
     toggler.classList.toggle('active');
 });
+
+
+// Header Active
+let mainHeader = document.getElementById('main-header');
+
+window.addEventListener('scroll', function (e) {
+    let scrollTop = window.pageYOffset;
+
+    if (scrollTop > 120) {
+        mainHeader.classList.add('active');
+    } else {
+        mainHeader.classList.remove('active');
+    }
+});
