55004fb59749deff68741c8f2d6e8627fbb3a439
Developer-Faras <forasuddin.freelancer@gmail.com>
2022-02-03 23:33:09 +0600

    Complete Design

--- js/import.js
+++ js/import.js
@@ -11,7 +11,7 @@ let local = './js/'
 
 // Import All Js Library
 requireLib('https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js');  // Bootstarp
-
+// requireLib('https://api.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.js'); //Mapbox
 
 
 // Import Main Js
